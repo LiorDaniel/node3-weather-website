514ecb618736fa136e94089da1cc687edd2df2c8
ליאור דניאל <u8801757@bsmch.net>
2022-01-24 18:47:22 +0200

    added weather data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,8 +3,8 @@ const forecast = (lat, lon, callback) => {
     const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + encodeURIComponent(lat) + '&lon=' + encodeURIComponent(lon) + '&limit=1&units=metric&appid=f9984aea7124c81ef04e9798339be580'
 
     axios.get(url).then(res => {
-        const {temp, feels_like}= res.data.main
-        callback(undefined, "it is currently "+temp+" degrees out, feels like "+feels_like+" degrees.")
+        const {temp, feels_like,humidity}= res.data.main
+        callback(undefined, "it is currently "+temp+" degrees out, feels like "+feels_like+" degrees. humifity at "+humidity+"%.")
     }).catch(error => {
         if (error.response !== undefined) {
             callback("unable to find location", undefined)
